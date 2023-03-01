beade40d82ceb12e72b36f9186309cc08084ae43
Lais Rodrigues <laisrmacedo@air-de-lais.speedport.ip>
2023-03-01 18:54:28 +0100

    Resoluçao dos exercicios da aula ao vivo

--- tests/UserBusiness/login.test.ts
+++ tests/UserBusiness/login.test.ts
@@ -1,5 +1,7 @@
 import { UserBusiness } from "../../src/business/UserBusiness"
 import { LoginInputDTO } from "../../src/dtos/userDTO"
+import { BadRequestError } from "../../src/errors/BadRequestError"
+import { NotFoundError } from "../../src/errors/NotFoundError"
 import { HashManagerMock } from "../mocks/HashManagerMock"
 import { IdGeneratorMock } from "../mocks/IdGeneratorMock"
 import { TokenManagerMock } from "../mocks/TokenManagerMock"
@@ -32,4 +34,72 @@ describe("login", () => {
         const response = await userBusiness.login(input)
         expect(response.token).toBe("token-mock-admin")
     })
+
+    test("deve disparar erro caso email nao seja uma string", async () => {
+    expect.assertions(2)
+        try {
+            const input: LoginInputDTO = {
+                email: null,
+                password: "bananinha"
+            }
+    
+            await userBusiness.login(input)
+        } catch (error) {
+            if(error instanceof BadRequestError){
+                expect(error.message).toBe("'email' deve ser string")
+                expect(error.statusCode).toBe(400)
+            }        
+        }
+    })
+
+    test("deve disparar erro caso email nao exista", async () => {
+    expect.assertions(2)
+        try {
+            const input: LoginInputDTO = {
+                email: "naoexiste@email.com",
+                password: "bananinha"
+            }
+    
+            await userBusiness.login(input)
+        } catch (error) {
+            if(error instanceof NotFoundError){
+                expect(error.message).toBe("'email' não cadastrado")
+                expect(error.statusCode).toBe(404)
+            }        
+        }
+    })
+
+    test("deve disparar erro caso a senha esteja incorreta", async () => {
+    expect.assertions(2)
+        try {
+            const input: LoginInputDTO = {
+                email: "normal@email.com",
+                password: "senhaincorreta"
+            }
+    
+            await userBusiness.login(input)
+        } catch (error) {
+            if(error instanceof BadRequestError){
+                expect(error.message).toBe("'password' incorreto")
+                expect(error.statusCode).toBe(400)
+            }        
+        }
+    })
+
+    // test("deve disparar erro caso email nao seja encontrado", async () => {
+    // expect.assertions(1)
+    //     // outro formato
+    //     const input: LoginInputDTO = {
+    //         email: null,
+    //         password: "hash-bananinha"
+    //     }
+
+    //     expect(async () => {
+    //         await userBusiness.login(input)
+    //     }).rejects.toThrow("'email' não cadastrado")
+        
+    //     expect(async () => {
+    //         await userBusiness.login(input)
+    //     }).rejects.toBeInstanceOf(BadRequestError)
+    // })
 })
--- tests/UserBusiness/signup.test.ts
+++ tests/UserBusiness/signup.test.ts
@@ -1,5 +1,6 @@
 import { UserBusiness } from "../../src/business/UserBusiness"
 import { SignupInputDTO } from "../../src/dtos/userDTO"
+import { BadRequestError } from "../../src/errors/BadRequestError"
 import { HashManagerMock } from "../mocks/HashManagerMock"
 import { IdGeneratorMock } from "../mocks/IdGeneratorMock"
 import { TokenManagerMock } from "../mocks/TokenManagerMock"
@@ -22,5 +23,42 @@ describe("signup", () => {
 
         const response = await userBusiness.signup(input)
         expect(response.token).toBe("token-mock-normal")
+    })
+
+    test("deve disparar erro caso name nao seja uma string", async () => {
+    expect.assertions(2)
+        try {
+            const input: SignupInputDTO = {
+                email: "example@email.com",
+                name: null,
+                password: "bananinha"
+            }
+    
+            await userBusiness.signup(input)
+        } catch (error) {
+            if(error instanceof BadRequestError){
+                expect(error.message).toBe("'name' deve ser string")
+                expect(error.statusCode).toBe(400)
+            }
+            
+        }
+        
+    })
+
+    test("deve disparar erro caso email fornecido ja tenha sido cadastrado", async () => {
+        // outro formato
+        const input: SignupInputDTO = {
+            email: "normal@email.com",
+            name: "Example Mock",
+            password: "bananinha"
+        }
+
+        expect(async () => {
+            await userBusiness.signup(input)
+        }).rejects.toThrow("'email' já existe")
+        
+        expect(async () => {
+            await userBusiness.signup(input)
+        }).rejects.toBeInstanceOf(BadRequestError)
     })
 })
